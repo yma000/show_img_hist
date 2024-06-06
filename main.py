9487fe26fce6f612bd5efad92b8518a2b7b1dec8
Toru Tamaki <tamaki.toru@nitech.ac.jp>
2024-06-06 09:51:37 +0900

    change caption again

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ def get_image():
 
     st.image(
         im,
-        caption='image from wikimedia commons, now at 2024/06/06',
+        caption='image from wikimedia commons, now at 2024/06/06, again',
         use_column_width=True
     )
 
