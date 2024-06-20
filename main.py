6e28a57bccaae187db6297d7a19aff8d9ba69d38
Masaki Yamauchi <m.yamauchi.659@stn.nitech.ac.jp>
2024-06-20 20:04:35 +0900

    reset

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ def get_image():
 
     st.image(
         im,
-        caption='image from wikimedia commons, now at 2024/06/06 afternoon with correct code',
+        caption='image from wikimedia commons',
         use_column_width=True
     )
 
