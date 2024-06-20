6cd09f6f819568fdeb7fa0659b0431583a88d5ac
Masaki Yamauchi <m.yamauchi.659@stn.nitech.ac.jp>
2024-06-20 21:03:39 +0900

    change caption text

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ def get_image():
 
     st.image(
         im,
-        caption='image from wikimedia commons',
+        caption='image from wikimedia commons, now at 2024/06/20',
         use_column_width=True
     )
 
